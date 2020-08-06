e9d495c95cd4d5fd5b5b89a93d3846395f164912
Guilherme Marinheiro Chaves <gcchaves@amazon.com>
2020-08-06 22:28:02 +0200

    Fixing ~/${ssh_user}
    Fix the "double" home created by "~/" + ${ssh_user} which will result in /home/user/user.

--- aws-ssm-ec2-proxy-command.sh
+++ aws-ssm-ec2-proxy-command.sh
@@ -51,8 +51,8 @@ aws ssm send-command \
   --document-name 'AWS-RunShellScript' \
   --comment "Add an SSH public key to authorized_keys for 60 seconds" \
   --parameters commands="\"
-    mkdir -p ~/${ssh_user}/.ssh
-    cd ~/${ssh_user}/.ssh || exit 1
+    mkdir -p /home/${ssh_user}/.ssh
+    cd /home/${ssh_user}/.ssh || exit 1
     authorized_key='${ssh_public_key} ssm-session'
     echo \\\"\${authorized_key}\\\" >> authorized_keys
     sleep 60
