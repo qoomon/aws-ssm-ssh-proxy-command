33e9e44783d6a32228586e8881685087191fb672
Jason Fah <52981451+jason-nabooki@users.noreply.github.com>
2022-01-20 09:02:47 +1000

    Fix(windows): remove space character

--- ec2-instance-connect/aws-ssm-ec2-proxy-command.ps1
+++ ec2-instance-connect/aws-ssm-ec2-proxy-command.ps1
@@ -13,7 +13,7 @@
 # Add following SSH Config Entry to ~/.ssh/config
 #   host i-* mi-*
 #     IdentityFile ~/.ssh/id_rsa
-#     ProxyCommand powershell .exe ~/.ssh/aws-ssm-ec2-proxy-command.ps1 %h %r %p ~/.ssh/id_rsa.pub
+#     ProxyCommand powershell.exe ~/.ssh/aws-ssm-ec2-proxy-command.ps1 %h %r %p ~/.ssh/id_rsa.pub
 #     StrictHostKeyChecking no
 #
 # Ensure SSM Permissions for Target Instance Profile
