9419fd4a7c54dc09a15d3af9514c4485200fc9aa
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-02-28 18:04:32 +0100

    Update aws-ssm-ec2-proxy-command.ps1

--- aws-ssm-ec2-proxy-command.ps1
+++ aws-ssm-ec2-proxy-command.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env pwsh
 ######## Source ################################################################
 #
 # https://github.com/qoomon/aws-ssm-ec2-proxy-command
@@ -13,7 +13,7 @@
 # Add following SSH Config Entry to ~/.ssh/config
 #   host i-* mi-*
 #     IdentityFile ~/.ssh/id_rsa
-#     ProxyCommand powershell .exe ~/.ssh/aws-ssm-ec2-proxy-command.ps1 %h %r %p ~/.ssh/id_rsa.pub
+#     ProxyCommand powershell.exe ~/.ssh/aws-ssm-ec2-proxy-command.ps1 %h %r %p ~/.ssh/id_rsa.pub
 #     StrictHostKeyChecking no
 #
 # Ensure SSM Permissions for Target Instance Profile
@@ -21,7 +21,7 @@
 #
 # Open SSH Connection
 #   ssh <INSTANCE_USER>@<INSTANCE_ID>
-#   
+#
 #   Ensure AWS CLI environment variables are set properly
 #   e.g. AWS_PROFILE='default' ssh ec2-user@i-xxxxxxxxxxxxxxxx
 #
@@ -49,26 +49,25 @@ if ($splitted_instance.Length -gt 1)
   $env:AWS_DEFAULT_REGION = $splitted_instance[1]
 }
 
-$authorized_key = "$ssh_public_key ssm-session"
-$script = @"
-\"
-mkdir -p ~$ssh_user/.ssh && cd ~$ssh_user/.ssh || exit 1
-
-echo '$authorized_key' >> authorized_keys
-
-sleep $ssh_public_key_timeout
-
-grep -v -F '$authorized_key' authorized_keys > .authorized_keys
-mv .authorized_keys authorized_keys
-\"
-"@
-
 Write-Output "Add public key $ssh_public_key_path for $ssh_user at instance $ec2_instance_id for $ssh_public_key_timeout seconds"
+$addPublicKeyScript = @"
+  \"
+  mkdir -p ~$ssh_user/.ssh && cd ~$ssh_user/.ssh || exit 1
+
+  authorized_key='$ssh_public_key ssm-session'
+  echo \\\"`$authorized_key\\\" >> authorized_keys
+
+  sleep $ssh_public_key_timeout
+
+  grep -v -F \\\"`$authorized_key\\\" authorized_keys > .authorized_keys
+  mv .authorized_keys authorized_keys
+  \"
+"@
 aws ssm send-command `
   --instance-ids "$ec2_instance_id" `
   --document-name 'AWS-RunShellScript' `
   --comment "Add an SSH public key to authorized_keys for $ssh_public_key_timeout seconds" `
-  --parameters commands="$script"
+  --parameters commands="$addPublicKeyScript"
 if($LASTEXITCODE -ne 0) { Write-Error "Failed to add public key with error $output" }
 
 Write-Output "Start ssm session to instance $ec2_instance_id"
