f9f9f9e829095cf689b17acdd1c532d757007d95
daknhh <hamburger1992@googlemail.com>
2024-03-13 14:29:51 +0100

    extending script
    -check if ec2 instance is running
    - start intance
    - adjust console log

--- aws-ssm-ec2-proxy-command.sh
+++ aws-ssm-ec2-proxy-command.sh
@@ -32,6 +32,8 @@
 set -eu
 
 REGION_SEPARATOR='--'
+MAX_ITERATION=5
+SLEEP_DURATION=5
 
 ec2_instance_id="$1"
 ssh_user="$2"
@@ -40,31 +42,73 @@ ssh_public_key_path="$4"
 ssh_public_key="$(cat "${ssh_public_key_path}")"
 ssh_public_key_timeout=60
 
+
+
 if [[ "${ec2_instance_id}" == *"${REGION_SEPARATOR}"* ]]
 then
   export AWS_DEFAULT_REGION="${ec2_instance_id##*${REGION_SEPARATOR}}"
   ec2_instance_id="${ec2_instance_id%%${REGION_SEPARATOR}*}"
 fi
 
->/dev/stderr echo "Add public key ${ssh_public_key_path} for ${ssh_user} at instance ${ec2_instance_id} for ${ssh_public_key_timeout} seconds"
-aws ssm send-command \
-  --instance-ids "${ec2_instance_id}" \
-  --document-name 'AWS-RunShellScript' \
-  --comment "Add an SSH public key to authorized_keys for ${ssh_public_key_timeout} seconds" \
-  --parameters commands="\"
-    mkdir -p ~${ssh_user}/.ssh && cd ~${ssh_user}/.ssh || exit 1
+function connect() {
+ >/dev/stderr echo "\n➕  Add public key ${ssh_public_key_path} for ${ssh_user} at instance ${ec2_instance_id} for ${ssh_public_key_timeout} seconds"
+  aws ssm send-command \
+    --instance-ids "${ec2_instance_id}" \
+    --document-name 'AWS-RunShellScript' \
+    --comment "Add an SSH public key to authorized_keys for ${ssh_public_key_timeout} seconds" \
+    --parameters commands="\"
+      mkdir -p ~${ssh_user}/.ssh && cd ~${ssh_user}/.ssh || exit 1
 
-    authorized_key='${ssh_public_key} ssm-session'
-    echo \\\"\${authorized_key}\\\" >> authorized_keys
+      authorized_key='${ssh_public_key} ssm-session'
+      echo \\\"\${authorized_key}\\\" >> authorized_keys
 
-    sleep ${ssh_public_key_timeout}
+      sleep ${ssh_public_key_timeout}
 
-    grep -v -F \\\"\${authorized_key}\\\" authorized_keys > .authorized_keys
-    mv .authorized_keys authorized_keys
-  \""
+      grep -v -F \\\"\${authorized_key}\\\" authorized_keys > .authorized_keys
+      mv .authorized_keys authorized_keys
+    \""
 
->/dev/stderr echo "Start ssm session to instance ${ec2_instance_id}"
-aws ssm start-session \
-  --target "${ec2_instance_id}" \
-  --document-name 'AWS-StartSSHSession' \
-  --parameters "portNumber=${ssh_port}"
+  >/dev/stderr echo "🔗  Connect to instance ${ec2_instance_id} using SSM"
+  aws ssm start-session \
+    --target "${ec2_instance_id}" \
+    --document-name 'AWS-StartSSHSession' \
+    --parameters "portNumber=${ssh_port}"
+}
+
+function start_instance(){
+ # Instance is offline - start the instance
+    >/dev/stderr echo "\n🚀 Starting ec2 Instance ${ec2_instance_id}"
+    aws ec2 start-instances --instance-ids $ec2_instance_id --profile ${AWS_PROFILE} --region ${AWS_REGION}
+    sleep ${SLEEP_DURATION}
+    COUNT=0
+    >/dev/stderr echo "  ⏳ Wait until ${ec2_instance_id} is running"
+    while [ ${COUNT} -le ${MAX_ITERATION} ]; do
+        STATUS=`aws ssm describe-instance-information --filters Key=InstanceIds,Values=${ec2_instance_id} --output text --query 'InstanceInformationList[0].PingStatus' --profile ${AWS_PROFILE} --region ${AWS_REGION}`
+        if [ ${STATUS} == 'Online' ]; then
+            break
+        fi
+        # Max attempts reached, exit
+        if [ ${COUNT} -eq ${MAX_ITERATION} ]; then
+            exit 1
+        else
+            >/dev/stderr echo "     ⁃  [${COUNT}|${MAX_ITERATION}] - retry in ${SLEEP_DURATION} seconds"
+            let COUNT=COUNT+1
+            sleep ${SLEEP_DURATION}
+        fi
+    done
+}
+
+
+>/dev/stderr echo "⚙️  Ec2 Proxy Command \n"
+>/dev/stderr echo "🧪 Check if instance ${ec2_instance_id} is running"
+STATUS=`aws ssm describe-instance-information --filters Key=InstanceIds,Values=${ec2_instance_id} --output text --query 'InstanceInformationList[0].PingStatus' --profile ${AWS_PROFILE} --region ${AWS_REGION}`
+
+# If the instance is online, start the session
+if [ $STATUS == 'Online' ]; then
+  >/dev/stderr echo "   − State: 🟢 ${STATUS}"
+  connect
+else
+  >/dev/stderr echo "   − State: 🔴 Offline"
+  start_instance
+  connect
+fi
